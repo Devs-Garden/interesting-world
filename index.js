6258f6f578b30a15cb83ee06985380faf3e0f0c5
unknown <syam2011@gmail.com>
2018-04-01 22:13:04 +0530

    added constructor

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = function(width, height) {
-  return width * height;
-};
+class InterestingWorld {
+	
+  constructor (API_KEY) {
+  	var obj =  {
+		'key': 2018, 
+		'thisDayCount': 1001,
+		'thisWeekCount': 2002,
+		'thisMonthCount': 3003,
+		'thisYearCount': 4004
+		};
+  	return obj;
+  }
+
+}
+
+module.exports = InterestingWorld;
---
+++ test.js
@@ -0,0 +1,5 @@
+var InterestingWorld = require('./index.js');
+
+var obj =  new InterestingWorld(2018);
+
+console.log(JSON.stringify(obj));
